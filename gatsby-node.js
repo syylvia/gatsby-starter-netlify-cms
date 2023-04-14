9ebd43c19ab29cf4fc0814b5bfef06d2243a04f1
yeyuxin <yuxin.ye@take2.health>
2023-04-14 15:52:22 +0800

    no message

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,85 +3,376 @@ const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createRedirect, createPage } = actions
 
-  return graphql(`
+  const careers = await graphql(`
     {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            id
-            fields {
-              slug
+      allMdx(filter: { fields: { slug: { regex: "/join-us/" } } }, sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+        }
+      }
+    }
+  `)
+
+  if (careers.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
+
+  const allMdxQuery = await graphql(`
+    {
+      allMdx(limit: 1000, filter: { frontmatter: { hide: { ne: true } } }) {
+        nodes {
+          id
+          fields {
+            slug
+          }
+          parent {
+            ... on File {
+              relativeDirectory
             }
-            frontmatter {
-              tags
-              templateKey
-            }
+          }
+          internal {
+            contentFilePath
+          }
+          frontmatter {
+            languages
+            date
           }
         }
       }
     }
-  `).then((result) => {
-    if (result.errors) {
-      result.errors.forEach((e) => console.error(e.toString()))
-      return Promise.reject(result.errors)
+  `)
+
+  if (allMdxQuery.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
+
+  const postTemplate = resolve(__dirname, 'src/templates/Post.js')
+  const tAndCTemplate = resolve(__dirname, 'src/templates/T&C.js')
+  const careerTemplate = resolve(__dirname, 'src/templates/Career.js')
+
+  const allMdxList = allMdxQuery.data.allMdx.nodes
+
+  allMdxList?.forEach((mdx) => {
+    let path = mdx.fields?.slug
+    if (!path) return
+    // if (
+    //   mdx.parent.relativeDirectory === 'health-tips' ||
+    //   mdx.parent.relativeDirectory === 'promotions' ||
+    //   mdx.parent.relativeDirectory === 'updates' ||
+    //   mdx.parent.relativeDirectory === 'campaign-page-posts'
+    // )
+    //   return
+    let component = null,
+      defer = false
+
+    switch (mdx.parent.relativeDirectory) {
+      case 'terms-and-conditions':
+        component = `${tAndCTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = true
+        break
+      case 'join-us':
+        component = `${careerTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = true
+        break
+      default:
+        component = `${postTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = moment(mdx?.frontmatter?.date)?.isBefore('2021-12-31')
+        break
     }
 
-    const posts = result.data.allMarkdownRemark.edges
+    mdx?.frontmatter?.languages?.forEach((lang) => {
+      // if (lang === defaultLanguage) {
+      //   createPage({
+      //     path: `${path}`,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: `${path}`,
+      //     },
+      //     // defer,
+      //   })
+      //   createRedirect({
+      //         fromPath: `/${lang}${path}`,
+      //         // redirectInBrowser: true,
+      //         isPermanent: true,
+      //         toPath: path,
+      //   })
+      // }
+      // else {
+      //   createPage({
+      //     path: `/${lang}${path}`,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: `/${lang}${path}`,
+      //     },
+      //     // defer,
+      //   })
+      // }
 
-    posts.forEach((edge) => {
-      const id = edge.node.id
       createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
+        path: `/${lang}${path}`,
+        component,
         context: {
-          id,
+          slug: path,
+          sectionPath: mdx.parent.relativeDirectory,
+          regex: `/${mdx.parent.relativeDirectory}/`,
+          id: mdx.id,
+          contentFilePath: mdx.internal.contentFilePath,
+          curPath: `/${lang}${path}`,
         },
+        // defer,
       })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach((edge) => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach((tag) => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
+      // if (lang === defaultLanguage)
+      //   createPage({
+      //     path,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: path,
+      //     },
+      //     // defer,
+      //   })
+      if (lang === defaultLanguage)
+        createRedirect({
+          fromPath: path,
+          // redirectInBrowser: true,
+          isPermanent: true,
+          toPath: `/${lang}${path}`,
+        })
     })
   })
-}
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
+  // For development env to show doc pages
+  if (process.env.GATSBY_ENV === 'development') {
+    const allDocQuery = await graphql(`
+      {
+        allMdx(limit: 1000, filter: { internal: { contentFilePath: { regex: "/docs/" } } }) {
+          nodes {
+            id
+            fields {
+              slug
+            }
+            internal {
+              contentFilePath
+            }
+          }
+        }
+      }
+    `)
+    if (allDocQuery.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
+    const docTemplate = resolve(__dirname, 'src/templates/Doc.js')
+    const allDocList = allDocQuery.data.allMdx.nodes
+    allDocList?.forEach((node) => {
+      let slug = node?.fields?.slug
+      if (!slug) return
+      createPage({
+        path: `/docs${slug}`,
+        component: `${docTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: {
+          slug,
+        },
+      })
     })
   }
 }
+
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createRedirect, createPage } = actions
+
+  const careers = await graphql(`
+    {
+      allMdx(filter: { fields: { slug: { regex: "/join-us/" } } }, sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+        }
+      }
+    }
+  `)
+
+  if (careers.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
+
+  const allMdxQuery = await graphql(`
+    {
+      allMdx(limit: 1000, filter: { frontmatter: { hide: { ne: true } } }) {
+        nodes {
+          id
+          fields {
+            slug
+          }
+          parent {
+            ... on File {
+              relativeDirectory
+            }
+          }
+          internal {
+            contentFilePath
+          }
+          frontmatter {
+            languages
+            date
+          }
+        }
+      }
+    }
+  `)
+
+  if (allMdxQuery.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
+
+  const postTemplate = resolve(__dirname, 'src/templates/Post.js')
+  const tAndCTemplate = resolve(__dirname, 'src/templates/T&C.js')
+  const careerTemplate = resolve(__dirname, 'src/templates/Career.js')
+
+  const allMdxList = allMdxQuery.data.allMdx.nodes
+
+  allMdxList?.forEach((mdx) => {
+    let path = mdx.fields?.slug
+    if (!path) return
+    // if (
+    //   mdx.parent.relativeDirectory === 'health-tips' ||
+    //   mdx.parent.relativeDirectory === 'promotions' ||
+    //   mdx.parent.relativeDirectory === 'updates' ||
+    //   mdx.parent.relativeDirectory === 'campaign-page-posts'
+    // )
+    //   return
+    let component = null,
+      defer = false
+
+    switch (mdx.parent.relativeDirectory) {
+      case 'terms-and-conditions':
+        component = `${tAndCTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = true
+        break
+      case 'join-us':
+        component = `${careerTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = true
+        break
+      default:
+        component = `${postTemplate}?__contentFilePath=${mdx.internal.contentFilePath}`
+        defer = moment(mdx?.frontmatter?.date)?.isBefore('2021-12-31')
+        break
+    }
+
+    mdx?.frontmatter?.languages?.forEach((lang) => {
+      // if (lang === defaultLanguage) {
+      //   createPage({
+      //     path: `${path}`,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: `${path}`,
+      //     },
+      //     // defer,
+      //   })
+      //   createRedirect({
+      //         fromPath: `/${lang}${path}`,
+      //         // redirectInBrowser: true,
+      //         isPermanent: true,
+      //         toPath: path,
+      //   })
+      // }
+      // else {
+      //   createPage({
+      //     path: `/${lang}${path}`,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: `/${lang}${path}`,
+      //     },
+      //     // defer,
+      //   })
+      // }
+
+      createPage({
+        path: `/${lang}${path}`,
+        component,
+        context: {
+          slug: path,
+          sectionPath: mdx.parent.relativeDirectory,
+          regex: `/${mdx.parent.relativeDirectory}/`,
+          id: mdx.id,
+          contentFilePath: mdx.internal.contentFilePath,
+          curPath: `/${lang}${path}`,
+        },
+        // defer,
+      })
+      // if (lang === defaultLanguage)
+      //   createPage({
+      //     path,
+      //     component,
+      //     context: {
+      //       slug: path,
+      //       sectionPath: mdx.parent.relativeDirectory,
+      //       regex: `/${mdx.parent.relativeDirectory}/`,
+      //       id: mdx.id,
+      //       contentFilePath: mdx.internal.contentFilePath,
+      //       curPath: path,
+      //     },
+      //     // defer,
+      //   })
+      if (lang === defaultLanguage)
+        createRedirect({
+          fromPath: path,
+          // redirectInBrowser: true,
+          isPermanent: true,
+          toPath: `/${lang}${path}`,
+        })
+    })
+  })
+
+  // For development env to show doc pages
+  if (process.env.GATSBY_ENV === 'development') {
+    const allDocQuery = await graphql(`
+      {
+        allMdx(limit: 1000, filter: { internal: { contentFilePath: { regex: "/docs/" } } }) {
+          nodes {
+            id
+            fields {
+              slug
+            }
+            internal {
+              contentFilePath
+            }
+          }
+        }
+      }
+    `)
+    if (allDocQuery.errors) return reporter.panicOnBuild(`Error while running GraphQL query.`)
+
+    const docTemplate = resolve(__dirname, 'src/templates/Doc.js')
+    const allDocList = allDocQuery.data.allMdx.nodes
+    allDocList?.forEach((node) => {
+      let slug = node?.fields?.slug
+      if (!slug) return
+      createPage({
+        path: `/docs${slug}`,
+        component: `${docTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: {
+          slug,
+        },
+      })
+    })
+  }
+}
+
